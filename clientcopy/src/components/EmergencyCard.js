2a3584203b9ae6f72c5d068eb3f7aeb8bfcfb7f2
0ShNa0 <shravani053btcse21@igdtuw.ac.in>
2024-05-11 18:07:59 +0530

    fontsss

--- clientcopy/src/components/EmergencyCard.js
+++ clientcopy/src/components/EmergencyCard.js
@@ -37,7 +37,7 @@ const EmergencyCard = ({emergency,steps}) => {
      }}>
        
           <CardHeader title={<Typography color="black"  justifyContent= 'center' display='flex' borderBottom= '3px solid black'
-       alignItems= 'center' fontSize="24px" fontFamily='Playfair Display'>{emergency}</Typography>}
+       alignItems= 'center' fontSize={{sx:'15px',md:'19px',lg:'24px'}} fontFamily='Playfair Display'>{emergency}</Typography>}
        />
      
           < CardContent  style={{ display: 'flex', justifyContent: 'space-between' }} >
@@ -48,11 +48,11 @@ const EmergencyCard = ({emergency,steps}) => {
      fontFamily='Playfair Display' float="right" position="relative" marginLeft="15px" >
     
  <div>
-   <Typography component="span" fontSize={{sx:'15px',md:'20px',lg:'28px'}} color="red" fontFamily='Playfair Display' alignContent={'center'} display='flex'>Steps to be taken</Typography>
+   <Typography component="span" fontSize={{sx:'12px',md:'16px',lg:'22px'}} color="red" fontFamily='Playfair Display' alignContent={'center'} display='flex'>Steps to be taken</Typography>
   
    { steps.map((step)=>(
    <React.Fragment>
-   <Typography component="span" fontSize={{sx:'15px',md:'20px',lg:'28px'}} color="black" fontFamily='Playfair Display'> * {step}</Typography>
+   <Typography component="span" fontSize={{sx:'12px',md:'16px',lg:'22px'}} color="black" fontFamily='Playfair Display'> * {step}</Typography>
    <br/>
    </React.Fragment>
 )
